9157b1d8d79550f077939a0d3e2a170b549cf191
aman2489 <aman1100vishnoi@gmail.com>
2026-02-18 20:36:37 +0530

    insta clone day 1 and 1st commit

---
+++ Insta-Clone/src/controllers/auth.controller.js
@@ -0,0 +1,112 @@
+const userModel = require("../models/user.model");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+async function registerController (req, res)  {
+    const {email, username, password, bio, profileImage} = req.body;
+
+    // const isUserExistsByEmail = await userModel.findOne({email});
+
+    // if(!isUserExistsByEmail){
+    //     return res.status(409).json({message: "User with same email already exists"});
+    // }
+
+    // const isUserExistsByUsername = userModel.findOne({username});
+
+    // if(!isUserExistsByUsername){
+    //     return res.status(409).json({message: "Username already exists"});
+    // }
+
+    //Optimized db query/check
+
+    const userAlreadyExists = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+
+    if(userAlreadyExists){
+        return res.status(409).json({
+            message:"User already exists " + (userAlreadyExists.email ? "Email already exists" : "Username already exists")
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+    const user = await userModel.create({
+        username,
+        email,
+        bio,
+        profileImage,
+        password: hash
+    })
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, {expiresIn: "1d"});
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+        message: "User registered successfully",
+        user: {
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profileImage: user.profileImage
+        }
+    })
+}
+
+async function loginController (req, res)  {
+    const {username, email, password} = req.body;
+
+    const user = await userModel.findOne({
+        $or: [
+            {
+                username: username
+            },
+            {
+                email: email
+            }
+        ]
+    })
+
+    if(!user){
+        return res.status(404).json({
+            message: "User not found"
+        })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+    const isPassValid = hash === user.password;
+
+    if(!isPassValid){
+        return res.status(401).json({
+            message: "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id
+    }, process.env.JWT_SECRET, {expiresIn: "1d"})
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message: "User loggedIn successfully",
+        user: {
+            username: user.username,
+            email: user.email,
+            bio: user.bio,
+            profileImage: user.profileImage
+        }
+    })
+}
+
+module.exports = {
+    registerController,
+    loginController
+}
