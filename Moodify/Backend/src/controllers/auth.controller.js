6b83851ecb794214198c3f67250410894abd7d3c
aman2489 <aman1100vishnoi@gmail.com>
2026-02-28 23:15:56 +0530

    implemented redis and token blacklisting with redis

--- Moodify/Backend/src/controllers/auth.controller.js
+++ Moodify/Backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const blacklistModel = require("../models/blacklist.model");
+const redis = require("../config/cache");
 
 async function registerController(req, res){
     const {username, email, password} = req.body;
@@ -102,9 +103,7 @@ async function logoutController(req, res) {
 
     res.clearCookie("token");
 
-    await blacklistModel.create({
-        token
-    });
+    await redis.set(token, Date.now().toString(), "EX", 60 * 60);
 
     return res.status(200).json({message: "User logged out successfully."});
 
--- Moodify/Backend/src/middlewares/auth.middleaware.js
+++ Moodify/Backend/src/middlewares/auth.middleaware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const redis = require("../config/cache")
 
 
 async function getUser(req, res, next) {
@@ -9,10 +10,15 @@ async function getUser(req, res, next) {
         return res.status(400).json({message: "Token not provided."});
     }
 
+    const isTokenBlacklisted = await redis.get(token);
+
+    if(isTokenBlacklisted){
+        return re.json(401).json({message: "Invalid Token"});
+    }
+
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
-        console.log(decoded)
         next();
     }catch(err){
         return res.status(401).json({
