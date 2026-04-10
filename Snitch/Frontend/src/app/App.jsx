399cf741cc40f0b1f9fd6cf0c5270fba509f651f
aman2489 <aman1100vishnoi@gmail.com>
2026-04-10 20:15:39 +0530

    feat: integrate react-router for routing and set up Redux provider in main application

--- Snitch/Frontend/src/app/App.jsx
+++ Snitch/Frontend/src/app/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { RouterProvider } from "react-router";
+import { router } from "../app.routes";
+
 
 function App() {
-  return <></>;
+  return <>
+    <RouterProvider router={router}/>
+  </>;
 }
 
 export default App;
