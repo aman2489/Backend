1b3dc8e64c1040b8dff0ad85b0fb5ca69be3bd34
aman2489 <aman1100vishnoi@gmail.com>
2026-05-02 20:36:56 +0530

    docker

--- Docker/Backend/src/app.js
+++ Docker/Backend/src/app.js
@@ -15,5 +15,14 @@ app.get("/api/data", (req, res) => {
     res.status(200).json(data);
 });
 
+app.get("/api/users", (req, res) => {
+    const users = [
+        {id: 1, name: "Alice"},
+        {id: 2, name: "Bob"},
+        {id: 3, name: "Charlie"}
+    ]
+    res.status(200).json(users);
+})
+
 
 export default app;
